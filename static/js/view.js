8d2dc3c4c61f05903343b3feedf09e19445d0e8b
kkh <2015132021@g.shingu.ac.kr>
2021-05-21 22:31:03 +0900

    js

--- static/js/view.js
+++ static/js/view.js
@@ -3,6 +3,7 @@ var page_state = 0;
 page_list = ["page_loading", "page_login", "page_join", "page_main", "page_information", "page_rent", "page_map", "page_camera"]
 
 for(var i = 1; i < page_list.length; i++){
+    console.log(page_list[i] + " is none")
     document.getElementById(page_list[i]).style.display="none";
 }
 
