08e5509ad4b0a693ad03168718daf4850c51593b
kkh <2015132021@g.shingu.ac.kr>
2021-05-21 22:09:20 +0900

    js view

--- static/js/view.js
+++ static/js/view.js
@@ -1,6 +1,10 @@
 var page = 0;
 
-document.getElementsByClassName("page").style.display="none";
+page = ["page_loading", "page_login", "page_join", "page_main", "page_information", "page_rent", "page_map", "page_camera"]
+
+for(var i = 0; i > page.length; i++){
+    document.getElementById(page[i]).style.display="none";
+}
 
 function refresh(){
     switch(page){
