68ba4463faae63157beae87cace20987781ef449
kkh <2015132021@g.shingu.ac.kr>
2021-05-21 22:53:52 +0900

    function

--- static/js/view.js
+++ static/js/view.js
@@ -1,11 +1,11 @@
-var page_state = 0;
+var page_state = "page_loading";
 
 page_list = ["page_loading", "page_login", "page_join", "page_main", "page_information", "page_rent", "page_map", "page_camera"]
 
-function refresh(num){
-    document.getElementById(page_list[page_state]).style.display="none";
-    document.getElementById(page_list[num]).style.display="block";
-    page_state = num
+function refresh(str){
+    document.getElementById(page_state).style.display="none";
+    document.getElementById(str).style.display="block";
+    page_state = str
 }
 
-refresh(0)
+refresh(page_list(0))
