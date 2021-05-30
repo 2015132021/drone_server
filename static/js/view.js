707b59599c3898c84f36e264839f5443bbc400ea
kkh <2015132021@g.shingu.ac.kr>
2021-05-31 08:03:56 +0900

    JS

--- static/js/view.js
+++ static/js/view.js
@@ -34,7 +34,7 @@ function login(){
 
     var result = restful(uris['client'], json, "GET");
 
-    console.log(typeof result)
+    console.log(typeof(result))
     var result_json = JSON.parse(result);
     if(result_json.error == 'False'){
         refresh(page_list[3])
