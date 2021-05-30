6c9d741684bebda5acd5b14b3b2624d2e1863df6
kkh <2015132021@g.shingu.ac.kr>
2021-05-31 07:31:42 +0900

    JS

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -15,12 +15,12 @@ function restful(uri, json, REST){
     xhr.open(REST, encodeURI(url), true);  // form태그안에 어트리뷰트 써준거와 같다.
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4 && xhr.status == 200){
-            console.log(xhr.responseText);
+            console.log(xhr.responseText.toString());
         }
     }
     xhr.send();
     if(xhr.status === 200) {       // 성공
-        console.log(xhr.responseText.toString());   // responseText -> response body안에 들어있는 값이 text로 들어있다.
+        console.log(xhr.responseText);   // responseText -> response body안에 들어있는 값이 text로 들어있다.
         return xhr.response
         }
     else {                      // response 실패
