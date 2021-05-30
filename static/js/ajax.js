f664bba13e707c1d8b305b65db5807e2d27d6490
kkh <2015132021@g.shingu.ac.kr>
2021-05-31 08:32:59 +0900

    JS

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -18,7 +18,7 @@ function parsing(str){
     return string
 }
 
-function restful(uri, json, REST, json){
+function restful(uri, json, REST, return_json){
     var url = uri + JSON.stringify(json);
     console.log("url : " + url)
     // XMLHttpRequest 객체의 인스턴스를 생성합니다.
@@ -28,7 +28,7 @@ function restful(uri, json, REST, json){
         // xhr 객체의 status 값을 검사한다.
         if (xhr.status === 200) {
           console.log(xhr.responseText)
-          json = JSON.parse(xhr.responseText)
+          return_json = JSON.parse(xhr.responseText)
         }
     }
     // open() 메서드는 요청을 준비하는 메서드입니다. (http 메서드, 데이터를 받아올 URL 경로, 비동기 여부)
