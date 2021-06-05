8aa73c854cb656e614363d9e083288e717b8827d
kkh <2015132021@g.shingu.ac.kr>
2021-06-05 16:19:12 +0900

    js

--- static/js/view.js
+++ static/js/view.js
@@ -230,14 +230,6 @@ function join(){
 var gps_stop = false
 var gps_isrun = false
 
-const timer = setInterval(() => {
-    if(gps_stop == true)
-    {
-        break;
-    }
-    console.log("123")
-})
-
 function rent(){
     refresh(page_list[5])
     document.getElementById("rent_stat").innerHTML="서칭중..."
@@ -257,6 +249,9 @@ function rent(){
 
             if(gps_isrun == false){
                 gps_isrun = true
+                setInterval(function(){
+                    console.log("123")
+                }, 1000)
             }
         },
         "failed" : function(return_json){
