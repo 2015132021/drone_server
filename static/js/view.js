fe73b3818351c010b691850d3f0f609f17ae523e
kkh <2015132021@g.shingu.ac.kr>
2021-06-05 16:28:26 +0900

    js

--- static/js/view.js
+++ static/js/view.js
@@ -251,6 +251,7 @@ function rent(){
                 gps_isrun = true
                 setInterval(function(){
                     console.log("123")
+                    getLocation();
                 }, 1000)
             }
         },
@@ -260,6 +261,23 @@ function rent(){
     }
     restful(restjson);
 }
+
+function getLocation() {
+    if (navigator.geolocation) { // GPS를 지원하면
+      navigator.geolocation.getCurrentPosition(function(position) {
+        alert(position.coords.latitude + ' ' + position.coords.longitude);
+      }, function(error) {
+        console.error(error);
+      }, {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: Infinity
+      });
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
+  }
+
 
 function myinfo(){
     json = {
