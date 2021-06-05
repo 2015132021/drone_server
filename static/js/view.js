730088ea7027dc9c1d3ee00dffac8b13b8ef8a06
kkh <2015132021@g.shingu.ac.kr>
2021-06-05 18:46:59 +0900

    js

--- static/js/view.js
+++ static/js/view.js
@@ -269,6 +269,13 @@ function getLocation() {
         console.log(position.coords.latitude + ' ' + position.coords.longitude);
         lat = position.coords.latitude
         lng = position.coords.longitude
+
+        var options = { //지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(lat, lng), //지도의 중심좌표.
+            level: 3 //지도의 레벨(확대, 축소 정도)
+        };
+        
+        var map = new kakao.maps.Map(container, options);
       }, function(error) {
         console.error(error);
       }, {
@@ -332,9 +339,4 @@ function tomain(){
 }
 
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(lat, lng), //지도의 중심좌표.
-	level: 3 //지도의 레벨(확대, 축소 정도)
-};
-
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+ //지도 생성 및 객체 리턴
